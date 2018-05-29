f650512c58307eef2c976b02ec53003288f3c283
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-05-29 15:04:15 +0500

    add POST create  route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'url#index'
   get '/:short_url', to: 'url#show'
+  post '/:url', to: 'url#create'
 end
