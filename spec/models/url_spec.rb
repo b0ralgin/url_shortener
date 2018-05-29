b9ed63a44ce2f1429df436b9ae165d8487b99454
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-05-29 18:00:43 +0500

    add tests

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -14,4 +14,36 @@ RSpec.describe Url, type: :model do
     url = build(:url, long_url: "abc")
     expect(url.valid?).not_to be true
   end
+
+  describe "short_url" do 
+    before :each do
+      @url_google = create(:url)
+      @url_google.save
+    end
+    it "should get 6 letters as a short_url" do
+      url = build(:url)
+      url.save
+      expect(url.short_url).to match(/\A[a-z\d]{6}\z/i)
+    end
+    it "should get 6 letters as a short_url" do
+      url = build(:url)
+      url.save
+      expect(url.short_url).to match(/\A[a-z\d]{6}\z/i)
+    end
+    it "#duplicates finds a duplicate" do
+      url = build(:url)
+      expect(url.duplicates).to eq @url_google
+    end
+
+    it "#new? false if there is no url in DB" do
+      url = build(:url, long_url: "http://ya.ru")
+      expect(url.new?).to eq true
+    end
+
+    it "#new? false if url exists in DB" do
+      url = build(:url)
+      expect(url.new?).to eq false
+    end
+
+  end
 end
