3027db768611abc3e3f539911c608a7dc9f0ab42
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-05-29 11:11:06 +0500

    add test about get long url

--- spec/controllers/url_controller_spec.rb
+++ spec/controllers/url_controller_spec.rb
@@ -1,10 +1,17 @@
 require "rails_helper"
 
 RSpec.describe UrlController, :type => :controller do
-  describe "GET /" do
+  describe "Index" do
     it "should work" do
       get :index
       expect(response).to have_http_status(:ok)
-    end 
+    end
+  end
+
+  describe "URL" do
+   it "should fail when GET url" do
+    get :long
+    expect(response).to have_http_status(:not_found)
+   end
   end
 end
