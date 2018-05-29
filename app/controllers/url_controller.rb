49b78e1c14dd1caefd0e632c7e5bf6be8472fee0
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-05-29 14:21:48 +0500

    add function show

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,10 +1,18 @@
 class UrlController < ApplicationController
-
+  before_action :find_url, only: [:show]
   def index 
     render json: {}, status: 200
   end
 
   def show 
-    render json: {}, status: 404
+    redirect_to @url.long_url unless @url
+    render status: 404
+  end
+
+
+  private 
+
+  def find_url
+    @url = Url.find_by_short_url(params[:short_url])
   end
 end
