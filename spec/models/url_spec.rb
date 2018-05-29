ab359e5e4afcea7ba2a9a959e1ce14090a696b02
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-05-29 17:53:03 +0500

    add model tests

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a url" do
+    url = build(:url, long_url: nil)
+    expect(url.valid?).not_to be true
+  end
+  it "is valid with a url" do
+    url = build(:url)
+    expect(url.valid?).to be true
+  end
+
+  it "is invalid with  an invalid url" do
+    url = build(:url, long_url: "abc")
+    expect(url.valid?).not_to be true
+  end
 end
