cbf5f31e721315c681108b4a9f71732b610fd89b
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-05-29 15:38:13 +0500

    change route name

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'url#index'
   get '/:short_url', to: 'url#show'
-  post '/', to: 'url#create'
+  post '/links', to: 'url#create'
 end
