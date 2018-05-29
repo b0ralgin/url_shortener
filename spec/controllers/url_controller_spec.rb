effb5a2e0bc204127563c2b79b971c7ba0ae10f0
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-05-29 14:16:10 +0500

    add test

--- spec/controllers/url_controller_spec.rb
+++ spec/controllers/url_controller_spec.rb
@@ -9,9 +9,17 @@ RSpec.describe UrlController, :type => :controller do
   end
 
   describe "GET show" do
-   it "should fail when GET unknown url" do
-    get :show, params: { short_url: 'aaa' }
-    expect(response).to have_http_status(:not_found)
-   end
+    it "should fail when GET unknown url" do
+      get :show, params: { short_url: 'aaa' }
+      expect(response).to have_http_status(:not_found)
+    end
+
+    before do
+      @url = create(:url)
+    end
+    it "should get full URL" do 
+      get :show, params: { short_url: @url.short_url}
+      expect(assigns(:url)).to eq @url
+    end
   end
 end
