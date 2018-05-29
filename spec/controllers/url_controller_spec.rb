514a7c1cabcc1cfb1395fd846221f562a5515d12
Kirill Borodulin <kirill.borodylin@gmail.com>
2018-05-29 11:41:22 +0500

    refactor tests

--- spec/controllers/url_controller_spec.rb
+++ spec/controllers/url_controller_spec.rb
@@ -1,16 +1,16 @@
 require "rails_helper"
 
 RSpec.describe UrlController, :type => :controller do
-  describe "Index" do
-    it "should work" do
+  describe "GET index" do
+    it "should get status OK" do
       get :index
       expect(response).to have_http_status(:ok)
     end
   end
 
-  describe "URL" do
-   it "should fail when GET url" do
-    get :long
+  describe "GET show" do
+   it "should fail when GET unknown url" do
+    get :show, params: { short_url: 'aaa' }
     expect(response).to have_http_status(:not_found)
    end
   end
