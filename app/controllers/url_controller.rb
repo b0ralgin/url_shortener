ec0df0f7fe646cbddedf5921f38305793b7e79fd
vladshkurin <shkurinvlad@gmail.com>
2018-05-29 16:48:46 +0300

    fix rubocop issues

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,5 +1,5 @@
 class UrlController < ApplicationController
-  def index 
+  def index
     render json: {}, status: 200
   end
 
@@ -8,25 +8,26 @@ class UrlController < ApplicationController
     if @url
       redirect_to @url.long_url
     else
-      render status: :not_found ,nothing: true
+      render status: :not_found, nothing: true
     end
   end
 
-  def create 
+  def create
     @url = Url.new(url_params)
-    if @url.new?     
-      if @url.save 
+    if @url.new?
+      if @url.save
         redirect_to @url.short_url
       else
-        render json: {error: "cannot save link" }, status: :bad_request
+        render json: { error: 'cannot save link' }, status: :bad_request
       end
-     
-    else 
+
+    else
       redirect_to @url.duplicates.short_url
     end
   end
 
-  private 
+  private
+
   def url_params
     params.require(:url).permit(:long_url)
   end
--- app/models/url.rb
+++ app/models/url.rb
@@ -3,19 +3,20 @@ class Url < ApplicationRecord
   validates_format_of :long_url,
     with: /\A(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
   before_create :generate_short_url
+
   def duplicates
     Url.find_by_long_url(self.long_url)
   end
-  def new? 
+
+  def new?
     duplicates.nil?
   end
 
-private 
- 
-def generate_short_url
-  chars = ['0'..'9','A'..'Z','a'..'z'].map{|range| range.to_a}.flatten
-  self.short_url = 6.times.map{chars.sample}.join
-  self.short_url = 6.times.map{chars.sample}.join until Url.find_by_short_url(self.short_url).nil?
-end
+  private
 
+  def generate_short_url
+    chars = ['0'..'9','A'..'Z','a'..'z'].map { |range| range.to_a }.flatten
+    self.short_url = 6.times.map { chars.sample }.join
+    self.short_url = 6.times.map { chars.sample }.join until Url.find_by_short_url(short_url).nil?
+  end
 end
